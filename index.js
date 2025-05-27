9a05626683b927b42c829c8d17ff4278f18e1b19
rafokez <raphael.k.business@gmail.com>
2025-05-27 13:17:07 -0300

    feat: adding routes using express

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
+app.get('/', (req, res) => {
 
-    switch (req.url) {
 
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1> Ola </h1>');
-            break
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Ola </h1>');
 
-        case '/users':
+});
 
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users:[{
-                    name: 'Hcode',
-                    email: 'contato@hcode.com.br',
-                    id:1
-                }]
+app.get('/users', (req, res) => {
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [{
+            name: 'Hcode',
+            email: 'contato@hcode.com.br',
+            id: 1
+        }]
 
-            }));
-
-            break
-
-    }
+    });
 
 })
 
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
-})
+});
