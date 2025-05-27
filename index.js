b9f5091e9ef50f289208e3c5e607d0604b18e235
rafokez <raphael.k.business@gmail.com>
2025-05-27 12:50:32 -0300

    feat: adding json responses

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const http = require('http');
 
-let server = http.createServer((req, res) =>{
+let server = http.createServer((req, res) => {
 
     console.log('URL:', req.url);
     console.log('METHOD:', req.method);
 
-    res.end('OK');
+    switch (req.url) {
+
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1> Ola </h1>');
+            break
+
+        case '/users':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                users:[{
+                    name: 'Hcode',
+                    email: 'contato@hcode.com.br',
+                    id:1
+                }]
+
+            }));
+
+            break
+
+    }
 
 })
 
-server.listen(3000, '127.0.0.1', ()=>{
+server.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
