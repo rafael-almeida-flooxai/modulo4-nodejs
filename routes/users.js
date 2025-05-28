91df4df3c8704ec974a358b30669eaa8436276cd
rafokez <raphael.k.business@gmail.com>
2025-05-28 12:41:42 -0300

    feat: changing the code using db.find

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,56 @@
 let NeDB = require('nedb');
 let db = new NeDB({
     filename: 'users.db',
-    autoload:true
+    autoload: true
 });
 
 
-module.exports = (app)=>{
+module.exports = (app) => {
 
 
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
 
-        });
-
-    });
-
-    app.post('/users', (req, res) => {
-
-        db.insert(req.body, (err, user)=>{
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
 
             if (err) {
-
-                console.log(`error: ${err}`);
+                console.log(`error: ${err}`)
                 res.status(400).json({
-                    error: err
+                    error:err
                 })
             } else {
                 
-                res.status(200).json(user);
-
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                })
             }
 
+        })
 
-        });
 
     });
+
+app.post('/users', (req, res) => {
+
+    db.insert(req.body, (err, user) => {
+
+        if (err) {
+
+            console.log(`error: ${err}`);
+            res.status(400).json({
+                error: err
+            })
+        } else {
+
+            res.status(200).json(user);
+
+        }
+
+
+    });
+
+});
 
 }
