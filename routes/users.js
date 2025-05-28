d0ee9c5142093abbbb1c4d55951dc81ac925a7b9
rafokez <raphael.k.business@gmail.com>
2025-05-28 12:26:10 -0300

    feat: installing nedb and fixing code

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload:true
+});
+
 
 module.exports = (app)=>{
 
@@ -19,7 +25,22 @@ module.exports = (app)=>{
 
     app.post('/users', (req, res) => {
 
-        res.json(req.body);
+        db.insert(req.body, (err, user)=>{
+
+            if (err) {
+
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+            } else {
+                
+                res.status(200).json(user);
+
+            }
+
+
+        });
 
     });
 
